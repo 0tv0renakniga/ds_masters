913798395d22cd5c00646bc2ac8569eeca5aec70
scotty <rroge036@gmail.com>
2025-04-20 23:16:31 -0600

    add turn in dir.. double check 2-4 idk

--- dsc_208/module_2/hw2-1.7.sql
+++ dsc_208/module_2/hw2-1.7.sql
@@ -3,28 +3,15 @@ Question 7: Write a query that returns the artist name and total number of track
 Blues bands.
 [Output relation cardinality: 5 rows]
 */
-
-/*
-Logic:
-1. Join the artists, albums, tracks, and genres tables.
-2. Filter the results to include only tracks that belong to the "Blues" genre.
-3. Group by artist name.
-4. Count the tracks for each artist.
-5. Return artist name and the count of tracks.
-*/
-
-SELECT
-  ar.Name,
-  COUNT(t.TrackId) AS TotalTracks
-FROM
-  artists ar
-JOIN
-  albums al ON ar.ArtistId = al.ArtistId
-JOIN
-  tracks t ON al.AlbumId = t.AlbumId
-JOIN
-  genres g ON t.GenreId = g.GenreId
-WHERE
-  g.Name = 'Blues'
-GROUP BY
-  ar.Name;
+-- Query : Question 7
+  SELECT ar.Name,
+         COUNT(t.TrackId) AS TotalTracks
+    FROM artists AS ar
+    JOIN albums AS al 
+      ON ar.ArtistId = al.ArtistId
+    JOIN tracks AS t 
+      ON al.AlbumId = t.AlbumId
+    JOIN genres AS g 
+      ON t.GenreId = g.GenreId
+   WHERE g.Name = 'Blues'
+GROUP BY ar.Name;
